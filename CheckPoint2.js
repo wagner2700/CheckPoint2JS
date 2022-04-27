4f2d9afa456d6a0b4b72fc0aa2d4ba2487ef7c51
wagner2700 <waguinhoxd4@gmail.com>
2022-04-27 20:27:59 -0300

    second commit

---
+++ CheckPoint2.js
@@ -0,0 +1,89 @@
+let alunos = []
+
+
+function Aluno(nome, qtdFaltas, notas) {
+  this.nome = nome
+  this.qtdFaltas = qtdFaltas
+  this.notas = notas
+  
+  // passo 2
+  this.calculaMedia = function() {
+      let aluno = {}
+      let notasTotal = this.notas.reduce((prev, curr) => prev + curr )
+
+      aluno.nome = this.nome
+      aluno.media = (notasTotal / this.notas.length).toFixed(2) 
+      aluno.faltas = this.qtdFaltas
+      aluno.notas = this.notas
+
+      return aluno
+  }
+
+  this.faltas = function() {
+      return this.qtdFaltas++
+  }
+}
+
+const Wagner   =  new Aluno('Wagner' , 20, [6,8,7,9])
+const Joao     =  new Aluno('Joao'   , 4, [9,8,8,9])
+const Luiz     =  new Aluno('Luiz'   , 0, [6,8,7,9])
+const Jose     =  new Aluno('Jose'   , 9, [2,3,1,0])
+const Maria    =  new Aluno('Maria'  , 7, [6,5,8,9])
+const Larissa  =  new Aluno('Larissa' , 15, [4,8,9,10])
+
+// passo 3
+let curso = {
+  nome: 'Dev',
+  faltasMax: 20,
+  notaAprovacao: 7,
+  listaEstudantes: [
+      Wagner, Joao, Luiz,
+      Jose, Maria, Larissa
+  ],
+  // Passo 4
+  novoAluno: function() {
+      this.listaEstudantes.push(new Aluno('Lala', 7, [7,5,3,8]))
+  },
+  // Passo 5
+  aprovado: function(aluno) {
+      let faltas = aluno.qtdFaltas
+      let notas = aluno.notas
+      let media =(notas.reduce((prev, curr) => prev + curr) / notas.length).toFixed(2)
+
+
+      if ( faltas <= this.faltasMax &&
+          media  >= this.notaAprovacao)  {
+          return true
+      }
+      if (faltas == this.faltasMax && 
+          media >= (this.notaAprovacao * 1.1) ){
+          
+          return true
+      }
+      return false
+  },
+  verificaAprovacao: function() {
+      this.listaEstudantes.forEach(aluno => {
+          if (this.aprovado(aluno)) {
+              aluno.aprovado = true
+          } else {
+              aluno.aprovado = false
+          }
+      })
+      console.table(this.listaEstudantes, ['nome', 'aprovado'])
+  }
+}
+
+console.table([
+  Wagner.calculaMedia(),
+  Joao.calculaMedia(),
+  Luiz.calculaMedia(),
+  Jose.calculaMedia(),
+  Maria.calculaMedia(),
+  Wagner.calculaMedia()
+], ['nome', 'notas', 'media', 'faltas'])
+
+curso.verificaAprovacao()
+
+
+
